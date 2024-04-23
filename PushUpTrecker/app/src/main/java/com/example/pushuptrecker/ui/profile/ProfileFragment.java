9c9688727dbe78ec5a2033ccd259dc4ac8f57513
Aleynik_Andrew <aaa205@tpu.ru>
2024-04-23 22:12:20 +0700

    .

--- PushUpTrecker/app/src/main/java/com/example/pushuptrecker/ui/profile/ProfileFragment.java
+++ PushUpTrecker/app/src/main/java/com/example/pushuptrecker/ui/profile/ProfileFragment.java
@@ -53,10 +53,6 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 nameEdt.getLayoutParams().
-                ageEdt;
-                sexEdt;
-                locationEdt;
-                linksLV;
             }
         });
 
