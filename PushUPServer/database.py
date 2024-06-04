1bcbe1b6144943ace9c5ea110645aad5f774791f
Olegan04 <o.evseev04@ya.ru>
2024-06-04 22:43:42 +0700

    update server

--- PushUPServer/database.py
+++ PushUPServer/database.py
@@ -1,5 +1,4 @@
 import psycopg2
-from users import Users
 from datetime import date
 from pandas.io.sql import DatabaseError
 #import cv2
@@ -15,15 +14,16 @@ def InsertUser(user):
         cur.execute(posgres, user)
         conn.commit() 
     except:
-        return "error adding a new user"
+        return '-101'
     
     posgres = "select id_user from users where binding = %s"
     cur.execute(posgres, (user[0],))
     p = cur.fetchall()
-    posgres = "INSERT INTO push_ups (id_user, id_training, colibration, quantity_per_day, quantity_per_mounth, quantity_per_year, quantity_per_all_time) VALUES (%s, 1, 0, 0, 0, 0, 0)"
-    cur.execute(posgres, p[0][0])
+    print(str(p[0][0]))
+    posgres = "INSERT INTO push_ups (id_user, id_training, calibration, quantity_per_day, quantity_per_month, quantity_per_year, quantity_per_all_time) VALUES (%s, 1, 0, 0, 0, 0, 0)"
+    cur.execute(posgres, (str(p[0][0]), ))
     conn.commit()  
-    return p[0][0]
+    return str(p[0][0])
 
 def getID(email, password):
     try:
@@ -31,13 +31,13 @@ def getID(email, password):
         cur.execute(posgres, (email,))
         p = cur.fetchall()
     except:
-        return "error user verification"
+        return -100
     if (len(p) == 0):
-        return -2
+        return '-2'
     elif(p[0][1] == password):
         return p[0][0]
     else:
-        return -1
+        return '-1'
     
 def InsertPhoto(idUser, photo):
     try:
@@ -45,7 +45,7 @@ def InsertPhoto(idUser, photo):
         cur.execute(posgres, (photo, idUser))
         conn.commit()
     except:
-        return 'error adding photo'
+        return -104
 
 def setInformation(idUser):
     try:
@@ -53,12 +53,12 @@ def setInformation(idUser):
         cur.execute(posgres, (idUser,))
         p = cur.fetchall()
     except:
-        return "error obtaining user information"
+        return -102
     return p[0]
 
 def setColibration(idUser):
     try:
-        posgres = "select calibration from push_ups where id_user = %s"
+        posgres = "select id_training, calibration from push_ups where id_user = %s"
         cur.execute(posgres, (idUser,))
         p = cur.fetchall()
     except:
@@ -76,59 +76,89 @@ def updateCalibration(idUser, idTraining, x):
  
 def updateQuantity(idUser, idTraining, x):
     try:
-        posgres = "select quantity_per_day, quantity_per_mounth, quantity_per_year, quantity_per_all_time from push_ups where id_user = %s and id_training = %s"
+        posgres = "select quantity_per_day, quantity_per_month, quantity_per_year, quantity_per_all_time from push_ups where id_user = %s and id_training = %s"
         cur.execute(posgres, (idUser, idTraining, ))
         p = cur.fetchall()
     except:
         return 'error adding in the number of pusp-ups'
+    pp = []
     for i in p[0]:
-        i += x
-    posgres = "UPDATE push_ups set quantity_per_day = %s, quantity_per_mounth = %s, quantity_per_year = %s, quantity_per_all_time = %s where id_user = %s and id_training = %s"
-    cur.execute(posgres, (p[0], idUser, idTraining))
+        pp.append(str(i + int(x)))
+    posgres = "UPDATE push_ups set quantity_per_day = %s, quantity_per_month = %s, quantity_per_year = %s, quantity_per_all_time = %s where id_user = %s and id_training = %s"
+    cur.execute(posgres, tuple(pp) + (idUser, idTraining,))
     conn.commit()
     return 'quantity of pusp-ups change'
 
 def  updateQuantityDay(idUser, x):
     try:
-        posgres = "UPDATE users set quantity_of_day = %s where is_user = %s"
-        cur.execute(posgres, (x, idUser))
+        posgres = "UPDATE users set quantity_of_day = %s where id_user = %s"
+        cur.execute(posgres, (x, idUser, ))
         conn.commit()
-    except:
-        return 'error adding quantity of day'
+    except DatabaseError as e:
+        return 'error' + str(e) 
     return 'quantity of day change'
 
 def updateData(idUser, user):
     try:
         posgres = "UPDATE users set name_user = %s, date_of_birth = %s, gender = %s, country = %s, location = %s where id_user = %s"
-        cur.execute(posgres, (user, idUser))
+        cur.execute(posgres, tuple(user) + (idUser, ))
         conn.commit()
-    except:
-        return 'error addind data user'
+    except DatabaseError as e:
+        return 'error addind data user' + str(e)
+    return 'yra'
     
 def extractQuantity(idUser, idTraining):
     try:
-        posgres = "SELECT quantity_per_day, quantity_per_mounth, quantity_per_year, quantity_per_all_time FROM push_ups where id_user = %s and id_training = %s"
+        posgres = "SELECT quantity_per_day, quantity_per_month, quantity_per_year, quantity_per_all_time FROM push_ups where id_user = %s and id_training = %s"
         cur.execute(posgres, (idUser, idTraining, ))
         p = cur.fetchall()
     except:
         return 'error extract quantity'
     return p[0]
 
-def setStatistic(idTraining, nameTraining, x, y):
+def setStatistic(idTraining, timeInterval, x, y):
     try:
-        posgres = ('''SELECT u.user_name, %s
-                FROM user u LEFT JOIN push_ups p
+        timeInterval = 'p.quantity_per_' + timeInterval
+        posgres = ('SELECT u.name_user, ' + timeInterval +'''
+                FROM users u JOIN push_ups p
                 ON u.id_user = p.id_user
                 WHERE p.id_training = %s
-                ORDER BY %s
-                OFFSET (SELECT COUNT(*) - %s FROM push_ups)
+                ORDER BY ''' + timeInterval + ''' DESC
+                OFFSET (%s)
                 LIMIT %s''')
-        nameTraining = 'p.' + nameTraining
-        cur.execute(posgres, (nameTraining, idTraining, nameTraining, x, y, ))
+        
+        cur.execute(posgres, (idTraining, x, y, ))
         p = cur.fetchall()
-    except:
-        return 'error getting ststistics'
+    except DatabaseError as e:
+        print (str(e))
+        return -110
     return p
+
+def addTraining(idUser, IdTraining):
+    try:
+        posgres = '''INSERT INTO push_ups 
+        (id_user, id_training, calibration, quantity_per_day, quantity_per_month, quantity_per_year, quantity_per_all_time)
+        VALUES (%s, %s, 0, 0, 0, 0, 0)'''
+        cur.execute(posgres, (idUser, IdTraining))
+        conn.commit()  
+    except DatabaseError as e:
+        return str(e)
+    return 'Ura'
+    
+def newDay():
+    posgres = 'UPDATE push_ups SET quantity_per_day = 0'
+    cur.execute(posgres)
+    conn.commit()
+
+def newMonth():
+    posgres = 'UPDATE push_ups SET quantity_per_month = 0'
+    cur.execute(posgres)
+    conn.commit()
+
+def newYear():
+    posgres = 'UPDATE push_ups SET quantity_per_year = 0'
+    cur.execute(posgres)
+    conn.commit()
 
 # cur.close()
 # conn.close()
--- PushUPServer/socket_server.py
+++ PushUPServer/socket_server.py
@@ -1,27 +1,36 @@
 import socket
 from database import *
 import re;
+import selectors
+import types
+import datetime
+import time
+import threading
 #import cv2
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("10.114.7.4", 3030))
-s.listen(1)
-conn, addr = s.accept()
+s.listen()
+s.setblocking(False)
+sel = selectors.DefaultSelector()
+sel.register(s, selectors.EVENT_READ, data=None)
+
+#conn, addr = s.accept()
 
 def getId(query):
     email, password = query.split('#')
     idUser = getID(email, password)
     answer = ''
-    if (idUser == -1):
+    if (idUser == '-1'):
         answer = 'error: incorrect password'
-    elif idUser == -2:
+    elif idUser == '-2':
         answer = 'error: user is not found'
     else:
-        answer = str(idUser)
-    return answer
+        answer = idUser
+    return str(answer)
 
 def getNewID(query):
     user = query.split('#')
-    answer = str(InsertUser(user))
+    answer = InsertUser(user)
     return answer
 
 def getInformation(idUser):
@@ -29,12 +38,12 @@ def getInformation(idUser):
     answer = ''
     for i in inf:
         answer += str(i) + '#'
-    answer = answer[:len(answer-1)] 
+    answer = answer[:len(answer)-1] 
     return answer
 
 def setDataUser(query):
-    idUser, user = query.split('#')
-    answer = updateData(idUser, user)
+    user = query.split('#')
+    answer = updateData(user[0], user[1:])
     return answer
 
 def setPhoto(query):
@@ -51,48 +60,83 @@ def getColibration(idUser):
     colibr = setColibration(idUser)
     answer = ''
     for i in colibr:
-        answer += i[0] + '#'
-    answer = answer[:len(answer-1)]
+        answer += str(i[0]) + '#' + str(i[1]) + '#'
+    answer = answer[:len(answer)-1]
     return answer
 
-def updateCalibration(query):
-    idUser, idTraining, x = query.splite('#')
+def update_Calibration(query):
+    idUser, idTraining, x = query.split('#')
     answer = updateCalibration(idUser, idTraining, x)
     return answer
 
 def getQuantity(query):
-    idUser, idTraining = query.splite('#')
+    idUser, idTraining = query.split('#')
     quantity = extractQuantity(idUser, idTraining)
     answer = ''
     for i in quantity:
         answer += str(i) + '#'
-    answer = answer[:len(answer-1)]
+    answer = answer[:len(answer)-1]
     return answer
 
 def setQuantity(query):
-    idUser, idTraining, x = query.splite('#')
+    idUser, idTraining, x = query.split('#')
     answer = updateQuantity(idUser, idTraining, x)
     return answer
 
 def getStatistic(query):
     answer = ''
-    idTraning, nameTraining, x, y = query.splite('#')
+    idTraning, nameTraining, x, y = query.split('#')
     statistic = setStatistic(idTraning, nameTraining, x, y)
+    if statistic == -110:
+        return 'error getting ststistics'
     for i in statistic:
         answer += str(i[0]) + '#' + str(i[1]) + '#'
-    answer = answer[:len(answer-1)]
+    answer = answer[:len(answer)-1]
     return answer
 
-while True:
-    data = conn.recv(1024) #прием
-    request = data.decode('utf-8')
-    code = int(request[0:3])    
-    request = re.sub(r'.','',request, count = 4)
-    print(request)
+def addNewNtaining(query):
+    idUser, idTraining = query.split('#')
+    return addTraining(idUser, idTraining) 
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        try:
+            recvData = sock.recv(1024)
+        except:
+            recvData = None
+         
+        if recvData:
+            request = recvData.decode('utf-8')
+            code = int(request[0:3])    
+            request = re.sub(r'.','',request, count = 4)
+            print(request)
+            answer = processingRequest(request, code)
+            print(answer)
+            data.outb += answer.encode('ascii')
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+def processingRequest (request, code):
     answer = ''
     match code:
         case 100:
-            answer = getID(request)
+            answer = getId(request)
         case 101:
             answer = getNewID(request)
         case 102:
@@ -106,15 +150,48 @@ while True:
         case 106:
             answer = getColibration(request)
         case 107:
-            answer = updateCalibration(request)
+            answer = update_Calibration(request)
         case 108:
             answer = getQuantity(request)
         case 109:
             answer = setQuantity(request)
         case 110:
             answer = getStatistic(request)
+        case 111:
+            answer = addNewNtaining(request)
     answer += '\n'
-    conn.send(answer.encode('ascii')) #отправка
-    print("Отправил ответ")
-conn.close()
+    return answer
+
+def checkTime():
+    currentDay = datetime.date.today()
+    currentMonth = datetime.datetime.now().month
+    currentYear = datetime.datetime.now().year
+    while True:
+        if datetime.date.today() != currentDay:
+            currentDay = datetime.date.today()
+            newDay()
+
+        if datetime.datetime.now().month != currentMonth:
+            currentMonth = datetime.date.today()
+            newMonth()
+
+        if datetime.datetime.now().year != currentYear:
+            currentYear = datetime.datetime.now().year
+            newYear()
+        
+        time.sleep(600)
+try:
+    while True:
+        thread = threading.Thread(target=checkTime)
+        thread.start()
+        data = sel.select(timeout=None)
+        for key, mask in data:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("пользователь отключился")
+finally:
+    conn.close()
 
